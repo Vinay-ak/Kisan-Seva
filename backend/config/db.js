2515896c2c81a843b6d618cfb643de22d497cb69
Vinayak Shree <vinayakshree2004@gmail.com>
2025-01-16 13:45:19 +0530

    Removed DB Link

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://vinayakshree:callme%401234@cluster0.rwyp3.mongodb.net/').then(()=>console.log("DB Connected"))
+    await mongoose.connect('').then(()=>console.log("DB Connected"))
 }
